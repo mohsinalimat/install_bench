3b8cc2bceb617cf8e11a345e15ee3b0a3fa40e2c
Armin Haberl <haberlarmin@gmx.at>
2022-01-20 23:04:46 +0100

    add some python code

--- install_bench.py
+++ install_bench.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 """
-This script installs
+This script installs frappe bench for you, creates a site with bench manager, and sets up production.
 """
 
+import os
 
+def main():
+    pass
 
 
 if __name__ == '__main__':
